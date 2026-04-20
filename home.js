409d3cb4209f57839042cc40ae0e469d73be3052
Nosuke314 <okadanosuke1213@gmail.com>
2026-04-20 15:42:10 +0900

    Update home.js

--- home.js
+++ home.js
@@ -1,3 +1,10 @@
+const btn = document.getElementById("menuBtn");
+const menu = document.getElementById("menu");
+
 btn.onclick = function() {
-  menu.classList.toggle("active");
+  if (menu.style.display === "block") {
+    menu.style.display = "none";
+  } else {
+    menu.style.display = "block";
+  }
 };
